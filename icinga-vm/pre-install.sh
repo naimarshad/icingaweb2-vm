76a615427d9bc4d0d1cf6fa2f9a2906352b3bdaf
Naeem Arshad <naimarshad@ubuntu-pc.localdomain.local>
2017-04-05 12:16:19 +0300

    Edited for root password & added icinga2-selinux package

--- icinga-vm/pre-install.sh
+++ icinga-vm/pre-install.sh
@@ -3,15 +3,15 @@
 #Installing icinga2 Repos, updating repo cache & installing latest packages
 echo "Installing icinga2 Repos, updating repo cache & installing latest packages"
 echo "######################################################################################################"
- yum -y install policycoreutils-python vim epel-release; yum clean all
- yum -y install http://packages.icinga.com/epel/7/release/noarch/icinga-rpm-release-7-1.el7.centos.noarch.rpm; yum clean all
- yum -y update; yum clean all; \
+ yum -y install policycoreutils-python vim epel-release
+ yum -y install http://packages.icinga.com/epel/7/release/noarch/icinga-rpm-release-7-1.el7.centos.noarch.rpm
+ yum -y update
 
 
 #Installing the required packages for icinga2 daemon (Apache, mariadb php etc.)
 echo "Installing the required packages for icinga2 daemon (Apache, mariadb php etc.)"
 echo "######################################################################################################"
-yum -y install rsyslog httpd nagios-plugins-all mariadb-server mariadb-libs mariadb icinga2 icinga2-doc icinga2-ido-mysql icingaweb2 icingacli php-ZendFramework php-ZendFramework-Db-Adapter-Pdo-Pgsql php-ZendFramework-Db-Adapter-Pdo-Mysql php-ldap
+yum -y install rsyslog httpd nagios-plugins-all mariadb-server mariadb-libs mariadb icinga2 icinga2-doc icinga2-selinux icinga2-ido-mysql icingaweb2 icingacli php-ZendFramework php-ZendFramework-Db-Adapter-Pdo-Pgsql php-ZendFramework-Db-Adapter-Pdo-Mysql php-ldap
 
 yum clean all;
 
@@ -35,11 +35,11 @@ sleep 10
 #creating database, users & assinging permissions for icinga2 monitoring schema and for the icinga web 2 schema
 echo "Creating database, users & assinging permissions for icinga2 monitoring schema and for the icinga web 2 schema"
 echo "######################################################################################################"
-mysqladmin -u root password rootsrv007
-mysql -uroot -prootsrv007 -e "CREATE DATABASE icinga2"
-mysql -uroot -prootsrv007 -e "GRANT ALL PRIVILEGES ON icinga2.* TO 'icinga2'@'localhost' IDENTIFIED BY 'IcingaPass'; FLUSH PRIVILEGES;"
-mysql -uroot -prootsrv007 icinga2 < /usr/share/icinga2-ido-mysql/schema/mysql.sql
-mysql -uroot -prootsrv007 -e "CREATE DATABASE IF NOT EXISTS icingaweb2 ; GRANT ALL ON icingaweb2.* TO icingaweb2@localhost IDENTIFIED BY 'icingaweb2';"
+mysqladmin -u root password rootpass
+mysql -uroot -prootpass -e "CREATE DATABASE icinga2"
+mysql -uroot -prootpass -e "GRANT ALL PRIVILEGES ON icinga2.* TO 'icinga2'@'localhost' IDENTIFIED BY 'IcingaPass'; FLUSH PRIVILEGES;"
+mysql -uroot -prootpass icinga2 < /usr/share/icinga2-ido-mysql/schema/mysql.sql
+mysql -uroot -prootpass -e "CREATE DATABASE IF NOT EXISTS icingaweb2 ; GRANT ALL ON icingaweb2.* TO icingaweb2@localhost IDENTIFIED BY 'icingaweb2';"
 mysql -uicingaweb2 -picingaweb2 icingaweb2 < /usr/share/doc/icingaweb2/schema/mysql.schema.sql
 mysql -uicingaweb2 -picingaweb2 icingaweb2 -e "INSERT INTO icingaweb_user (name, active, password_hash) VALUES ('webadmin', 1, '\$1\$Ncq7Am5z\$AZ.88KH3RleyxDDh6vUr11');"
 
@@ -56,7 +56,7 @@ echo "Adding ports to open for connection to the running services for apache, rs
 echo "######################################################################################################"
 firewall-cmd --permanent --add-port=80/tcp
 firewall-cmd --permanent --add-port=514/tcp
-firewall-cmd --permanent --add-port=4665/tcp
+firewall-cmd --permanent --add-port=5665/tcp
 firewall-cmd --reload
 
 #setting selinux contexts to let /etc/icingaweb2 directory for read & write permissons by http process
